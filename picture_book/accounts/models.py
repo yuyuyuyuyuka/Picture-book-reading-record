4afbd93c3e86ba88b54794361a7eb057fe0d9396
yuyuyuyuyuka <takamichaan0915@gmail.com>
2025-03-25 04:22:49 +0900

    models.py記入（ユーザー作成）

--- picture_book/accounts/models.py
+++ picture_book/accounts/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import(
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('メールアドレスを入力してください')
+        if not password:
+            raise ValueError('パスワードを入力してください')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username,**extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields['is_staff'] = True
+        extra_fields['is_active'] = True
+        extra_fields['is_superuser'] = True
+        return self.create_user(email, username, password, **extra_fields)
+    
+
+# ユーザーモデル
+class User(AbstractBaseUser, PermissionsMixin):
+    family_id = models.ForeignKey('Family', on_delete=models.SET_NULL, null=True, blank=True)
+    username = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    reset_token = models.CharField(max_length=255, null=True, blank=True)
+    reset_token_expiry = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    def __str__(self):
+        return self.email
