059183cd9a2ae4c0a6d4e8ea7e8da80cead48c2f
yuyuyuyuyuka <takamichaan0915@gmail.com>
2025-03-25 12:42:16 +0900

    RegistForm作成

--- picture_book/accounts/forms.py
+++ picture_book/accounts/forms.py
@@ -1,3 +1,13 @@
 from django import forms
 from .models import User
 from django.contrib.auth.forms import AuthenticationForm
+
+
+class RegistForm(forms.ModelForm):
+    
+    class Meta():
+        model = User
+        fields = ['username', 'email', 'password']
+        widgets = {
+            'password':forms.PasswordInput
+        }
