b7aa5740f77370620cff36d02c047c8f0276c38c
yuyuyuyuyuka <takamichaan0915@gmail.com>
2025-03-25 13:39:47 +0900

    新規登録のforms.pyとaccountsのurls.pyを少し記入

--- picture_book/accounts/forms.py
+++ picture_book/accounts/forms.py
@@ -1,13 +1,21 @@
 from django import forms
 from .models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 
 
-class RegistForm(forms.ModelForm):
+class RegistForm(UserCreationForm):
     
-    class Meta():
+    class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password1','password2']
         widgets = {
-            'password':forms.PasswordInput
+            'password1':forms.PasswordInput(),
+            'password2':forms.PasswordInput()
+        }
+        labels = {
+            'username':'名前／ニックネーム',
+            'email':'メールアドレス',
+            'password1':'パスワード',
+            'password2':'パスワード(再入力)',
         }
