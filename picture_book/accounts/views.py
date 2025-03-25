37e55ee058c0d3fc824478f8f2a1528456e52ae0
yuyuyuyuyuka <takamichaan0915@gmail.com>
2025-03-26 05:10:41 +0900

    views.pyのログイン作成とtemplatesにloginファイル作成

--- picture_book/accounts/views.py
+++ picture_book/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .forms import RegistForm
-from django.contrib.auth.password_validation import validate_password
+from .forms import RegistForm, UserLoginForm
+from django.contrib.auth import authenticate, login, logout
+
 
 def regist(request):
     user_form = RegistForm(request.POST or None)
@@ -10,4 +11,15 @@ def regist(request):
         user_form = RegistForm()
     return render(request, 'accounts/registration.html', context={
         'user_form': user_form
+    })
+    
+def user_login(request):
+    login_form = UserLoginForm(request.POST or None)
+    if login_form.is_valid():
+        email = login_form.cleaned_data.get('email')
+        password = login_form.cleaned_data.get('password')
+        # 認証処理
+        user = authenticate(request, email=email, password=password)
+    return render(request, 'accounts/login.html', context={
+        'login_form': login_form
     })
