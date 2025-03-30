407a206b38aec71c9ecf3a6d2387f58cdbf815fe
GomaMona <goma.masa.0220@gmail.com>
2025-03-30 15:55:50 +0900

    PasswordResetTokenモデルを追加

--- picture_book/picture_book_app/models.py
+++ picture_book/picture_book_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
 
-# Create your models here.
+User = get_user_model()
+
+# PasswordResetTokenのモデルが必要
+class PasswordResetToken(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.UUIDField(default=uuid.uuid4, unique=True)
+    used = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.token}"
