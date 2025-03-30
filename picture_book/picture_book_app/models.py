60c08ef61fbd4a68b322b8e6905f282eda020d6d
yuyuyuyuyuka <takamichaan0915@gmail.com>
2025-03-31 06:27:32 +0900

    子どものモデル作成

--- picture_book/picture_book_app/models.py
+++ picture_book/picture_book_app/models.py
@@ -1,1 +1,13 @@
+from django.db import models
+
+# 子どもモデル
+class Child(models.Model):
+    family = models.ForeignKey('accounts.Family', on_delete=models.SET_NULL, null=True, blank=True, related_name='children')
+    name = models.CharField(max_length=225)
+    birthday = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
