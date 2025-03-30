b8c6fd931efdebfd345edd41ec4712b06516061a
GomaMona <goma.masa.0220@gmail.com>
2025-03-30 19:23:13 +0900

    accountsのmodels.pyにモデルがあったので内容リセット

--- picture_book/picture_book_app/models.py
+++ picture_book/picture_book_app/models.py
@@ -1,15 +1,3 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-import uuid
 
-User = get_user_model()
-
-# PasswordResetTokenのモデルが必要
-class PasswordResetToken(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    token = models.UUIDField(default=uuid.uuid4, unique=True)
-    used = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.user.username} - {self.token}"
+# Create your models here.
