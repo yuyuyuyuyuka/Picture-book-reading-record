b78fe947c7269732d83379b107bad71433328c77
yuyuyuyuyuka <takamichaan0915@gmail.com>
2025-04-05 18:38:57 +0900

    admin.py作成

--- picture_book/accounts/admin.py
+++ picture_book/accounts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from .models import PasswordResetToken, Family, Invitation
 
-# Register your models here.
+User = get_user_model()
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'is_active', 'is_staff', 'created_at', 'update_at')
+    search_fields = ('email', 'username')
+    list_filter = ('is_active', 'is_staff')
+    
+admin.site.register(User, UserAdmin)
+
+class PasswordResetTokenAdmin(admin.ModelAdmin):
+    list_display = ('user', 'token', 'used', 'created_at')
+    search_fields = ('user__email', 'token')
+    
+admin.site.register(PasswordResetToken, PasswordResetTokenAdmin)
+
+
+class FamilyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_at', 'update_at')
+    search_fields = ('id',)
+    
+admin.site.register(Family, FamilyAdmin)
+
+
+class InvitationAdmin(admin.ModelAdmin):
+    list_display = ('invite_token', 'family_id', 'user_id', 'used', 'expiry_date', 'created_at', 'update_at')
+    search_fields = ('invite_token',)
+    
+admin.site.register(Invitation, InvitationAdmin)
