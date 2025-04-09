a0e6c71e6660741fcff7167c5847db8b2f303ef9
yuyuyuyuyuka <takamichaan0915@gmail.com>
2025-04-09 21:59:48 +0900

    絵本のモデル作成

--- picture_book/picture_book_app/admin.py
+++ picture_book/picture_book_app/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Child
+from .models import Child, Book
 
 class ChildAdmin(admin.ModelAdmin):
     list_display = ('name', 'birthday', 'family_id', 'created_at', 'update_at')
     search_fields = ['name']
     
 admin.site.register(Child, ChildAdmin)
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publisher', 'cover_image', 'created_at', 'update_at')
+    search_fields = ['title']
+    
+admin.site.register(Book, BookAdmin)
--- picture_book/picture_book_app/models.py
+++ picture_book/picture_book_app/models.py
@@ -11,3 +11,14 @@ class Child(models.Model):
     def __str__(self):
         return self.name
 
+# 絵本モデル
+class Book(models.Model):
+    title = models.CharField(max_length=225)
+    author = models.CharField(max_length=225)
+    publisher = models.CharField (max_length=225)
+    cover_image = models.ImageField(upload_to='book_covers/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
