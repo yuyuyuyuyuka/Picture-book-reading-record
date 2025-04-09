5d06dd3bff77e979abf1d9acc89bf14392665773
yuyuyuyuyuka <takamichaan0915@gmail.com>
2025-04-09 12:18:13 +0900

    子どもの登録画面を作成中（お迎え等があるのでいったん現状を）

---
+++ picture_book/picture_book_app/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Child
+
+# 子どもの登録画面
+class ChildForm(forms.ModelForm):
+    
+    class Meta:
+        model = Child
+        fields = ['name', 'birthday']
+        labels = {
+            'name':'名前／ニックネーム',
+            'birthday':'誕生日',
+        }
+        # 誕生日入力をカレンダー表示
+        widgets = {
+            'birthday': forms.DateField(attrs={'type': 'date'}),
+        }
--- picture_book/picture_book_app/views.py
+++ picture_book/picture_book_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from .models import Child
+from .forms import ChildForm
 
 class HomeView(TemplateView):
     template_name = 'picture_book_app/home.html'
@@ -11,4 +12,18 @@ def child_list(request):
     children = Child.objects.filter(family_id=request.user.family_id)
     return render(request, 'picture_book_app/child_list.html', context={
         'children':children,
+    })
+    
+# 子ども登録画面
+def child_regist(request):
+    if request.method == 'POST':
+        form = ChildForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect ('picture_book_app:child_list')
+        
+    else:
+        form = ChildForm()
+    return render(request, 'picture_book_app/child_registration.html', context={
+        'form': form,
     })
