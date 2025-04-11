de34d92320c800aececba01106065d44e5e1043e
yuyuyuyuyuka <takamichaan0915@gmail.com>
2025-04-11 21:57:14 +0900

    絵本の読み聞かせ記録モデルと簡単記録のモデル作成

--- picture_book/picture_book_app/models.py
+++ picture_book/picture_book_app/models.py
@@ -22,3 +22,39 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+# 読み聞かせ記録モデル
+class ReadingRecord(models.Model):
+    child = models.ForeignKey(Child, on_delete=models.CASCADE, related_name='reading_records')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reading_records')
+    reading_comment =models.ManyToManyField(ReadingComment, blank=True)
+    date = models.DateField()
+    read_count = models.PositiveIntegerField()
+    
+    BOOK_STATUS_CHOICES = (
+        (0, '絵本持っている'),
+        (1, '図書館等で借りた')
+    )
+    
+    book_status = models.IntegerField(choices=BOOK_STATUS_CHOICES)  #0:絵本持っている1:図書館等で借りた
+    photo = models.ImageField(upload_to='reading_photos/', null=True, blank=True)  #子どもの写真
+    video = models.FileField(upload_to='reading_videos/', null=True, blank=True)  #子どもの動画
+    review = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.child.name} - {self.book.title}({self.data})"
+    
+
+
+#簡単記録モデル
+class ReadingComment(models.Model):
+    # 16項目ほど選択を作りたい
+    comment = models.CharField(max_length=225)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.comment
