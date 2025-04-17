48ae129ad5d5b8c68ff20da301b701c4e7dd7141
yuyuyuyuyuka <takamichaan0915@gmail.com>
2025-04-17 12:53:25 +0900

    エラー修正

--- accounts/views.py
+++ accounts/views.py
@@ -71,7 +71,9 @@ def send_password_reset_email(to_email_address, reset_url):
     subject = '【お話の足跡】パスワード再設定のお知らせ'
     content = Content("text/plain", f"以下のリンクからパスワードを再設定してください。\n\n{reset_url}")
 
-    message = Mail(from_email=from_email, to_email=to_email, subject=subject, plain_text_content=content)
+    message = Mail(from_email=from_email, to_email=to_email, subject=subject)
+    message.add_content(content)  # ← ここがポイント！
+
     response = sg.send(message)
     return response
 
